58a96c3090ef537ef800441ee30c621d2181e018
matokun95 <matias.jca95@gmail.com>
2022-05-06 21:33:26 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import { Formik, Form, Field } from 'formik'
+import './Styles/header.css'
+import './Styles/content.css'
+import './Styles/article.css'
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
-function App() {
+
+const App = () => {
+  const [photos, setPhotos] = useState([])
+  const openPhoto = (url) => window.open(url)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div>
+      <header>
+        <Formik
+          initialValues={{ search: '' }}
+          onSubmit={async values => {
+            const response = await fetch(`https://api.unsplash.com/search/photos?per_page=20&query=${values.search}`, {
+              headers: {
+                'Authorization': 'Client-ID rXPkNgsB29-uynn0vY_HuzFyCETLXKVj42CELIlyQBs'
+              }
+            })
+            const data = await response.json()
+            setPhotos(data.results)
+          }}
         >
-          Learn React
-        </a>
+          <Form>
+            <div className='form-content'>
+              <Field name='search' placeholder='Buscar' />
+              <button className='submit-button'>
+                <FontAwesomeIcon icon={faMagnifyingGlass} />
+              </button>
+            </div>
+          </Form>
+        </Formik>
       </header>
-    </div>
-  );
+      <div className='container'>
+        <div className='center'>
+          {photos.map(photo =>
+            <article key={photo.id} onClick={() => openPhoto(photo.links.html)}>
+              <img src={photo.urls.regular} />
+              <p> {[photo.description, photo.alt_description].join('-')} </p>
+            </article>
+          )}
+        </div>
+      </div>
+    </div >
+  )
 }
 
 export default App;
